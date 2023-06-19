193a148aae9e5dfc2b95ca6cc7356143b944c1be
KavyaP03 <136174355+KavyaP03@users.noreply.github.com>
2023-06-19 19:34:38 +0530

    Update Mycaptain Assignment .py

--- Mycaptain Assignment .py
+++ Mycaptain Assignment .py
@@ -1,1 +1,9 @@
+#Task 1
+pi=3.141592653589793
+r=float(input("Input the radius of the circle :"))
+print("The area of the circle with radius "+str(r)+" is:"+str(pi*r**2))
 
+#Task 2
+filename=input("Input the filename :")
+f_extens=filemane.split(".")
+print("The extension of the file is:"+repr(f_extens[-1]))
